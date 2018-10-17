a2781425e7ccda7f06b29768230a0b6de2bf11d6
Rafael Moraes <roliveira.moraes@gmail.com>
2018-10-17 11:42:13 -0300

    fix problem to create the font directory

--- debian/installers/install_font_roboto_mono_for_powerline.sh
+++ debian/installers/install_font_roboto_mono_for_powerline.sh
@@ -2,9 +2,9 @@
 
 FONT_URL=${font_url:-"https://raw.githubusercontent.com/powerline/fonts/master/RobotoMono/Roboto%20Mono%20for%20Powerline.ttf"}
 CONFIG_URL=${config_url:-"https://raw.githubusercontent.com/rafaelmoraes/dockerfiles/master/vscode/local.conf"}
-FONT_DIR=${font_dir:-"~/.local/share/fonts"}
+FONT_DIR=${font_dir:-"$HOME/.local/share/fonts"}
 
-mkdir -p "$FONT_DIR"
+mkdir -p "$FONT_DIR/"
 curl -fLo "$FONT_DIR/Roboto Mono for Powerline.ttf" "$FONT_URL"
 
 curl -o /etc/fonts/local.conf "$CONFIG_URL"
