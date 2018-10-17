80727950fca21f450c7bccab4807e531b27895b4
Rafael Moraes <roliveira.moraes@gmail.com>
2018-10-17 13:10:41 -0300

    fixed all bugs, now it`s works

--- debian/installers/install_font_roboto_mono_for_powerline.sh
+++ debian/installers/install_font_roboto_mono_for_powerline.sh
@@ -4,8 +4,18 @@ FONT_URL=${font_url:-"https://raw.githubusercontent.com/powerline/fonts/master/R
 CONFIG_URL=${config_url:-"https://raw.githubusercontent.com/rafaelmoraes/dockerfiles/master/vscode/local.conf"}
 FONT_DIR=${font_dir:-"$HOME/.local/share/fonts"}
 
-mkdir -p "$FONT_DIR/"
+mkdir -p $FONT_DIR
+
+if [ ! -x curl ]; then
+    apt-get update && apt-get install curl -y
+fi
+
 curl -fLo "$FONT_DIR/Roboto Mono for Powerline.ttf" "$FONT_URL"
 
+mkdir -p /etc/fonts/
+
 curl -o /etc/fonts/local.conf "$CONFIG_URL"
-fc-cache -vf "$FONT_DIR"
+
+if [ -x fc-cache ];then
+    fc-cache -vf "$FONT_DIR"
+fi
