6fc6afe31f065df05c7b0ede540696ff191fb9cd
Rafael Moraes <roliveira.moraes@gmail.com>
2019-01-25 11:05:40 -0200

    moved keyboard and touchpad to all/configurators

--- all/configurators/touchpad.sh
+++ all/configurators/touchpad.sh
@@ -6,8 +6,8 @@
 #
 #
 # :AUTHORS: Rafael Moraes <roliveira.moraes@gmail.com>
-# :DATE: 2019-01-22
-# :VERSION: 0.0.1
+# :DATE: 2019-01-25
+# :VERSION: 0.0.2
 ##############################################################################
 
 set -euo pipefail
@@ -17,9 +17,15 @@ exit_is_not_superuser() {
     if [ "$(id -u)" != "0" ]; then w_echo "Run as root or using sudo."; exit 1; fi
 }
 
-install_requirements() {
-    apt update
-    apt install -y xinput
+try_install_requirements_if_needed() {
+    if [ -z "$(which xinput)" ]; then
+        if [ -x "$(which apt)" ]; then
+            apt update
+            apt install -y xinput
+        else
+            w_echo 'You need to install: xinput'
+        fi
+    fi
 }
 
 apply_configuration() {
@@ -32,7 +38,7 @@ apply_configuration() {
 main() {
     exit_is_not_superuser
     i_echo 'Configure Dell Vostro 14-5480 Touchpad'
-    install_requirements
+    try_install_requirements_if_needed
     apply_configuration
     i_echo "Touchpad configured successfully"
 }
