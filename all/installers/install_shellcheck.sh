774d1b031026e49db31451a6b6fcf3c0afb79346
Rafael Moraes <roliveira.moraes@gmail.com>
2019-02-05 16:52:46 -0200

    Refactor without impact on the public API

--- all/installers/install_shellcheck.sh
+++ all/installers/install_shellcheck.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 ##############################################################################
 # install_shellcheck.sh
 # -----------
@@ -7,7 +7,7 @@
 #
 # :AUTHORS: Rafael Moraes <roliveira.moraes@gmail.com>
 # :DATE: 2019-01-31
-# :VERSION: 0.0.1
+# :VERSION: 0.0.2
 ##############################################################################
 
 set -euo pipefail
@@ -20,41 +20,71 @@ exit_is_not_superuser() {
     if [ "$(id -u)" != "0" ]; then w_echo "Run as root or using sudo."; exit 1; fi
 }
 
-alpine_install_shellcheck() {
-    if [ -x "$(which apk)" ]; then
-        apk update
-        apk add --no-cache \
-                curl \
-                cabal \
-                ghc \
-                build-base
-
-        cabal update
-        cabal install ShellCheck || true
-        ln -fs "$HOME/.cabal/bin/shellcheck" /usr/local/bin/shellcheck
-        if [ -x "$(which shellcheck)" ]; then
-            exit 0
+shellcheck_is_installed() {
+    if [ -x "$(which shellcheck)" ]; then
+        shellcheck --version &>/dev/null
+        if [ "$?" == "0" ]; then
+            echo true
         else
-            exit 1
+            echo false
         fi
+    else
+        echo false
     fi
+}
+
+alpine_install_shellcheck() {
+    apk update
+    apk add --no-cache \
+            curl \
+            cabal \
+            ghc \
+            build-base
+    cabal update
+    cabal install ShellCheck || true
+    cp -f "$HOME/.cabal/bin/shellcheck" /usr/local/bin/shellcheck
+}
+
+alpine_clear_up(){
+    apk del cabal \
+            ghc \
+            build-base
+
+    # rm -rf "$HOME/.cabal"
+    # rm -rf "$HOME/.ghc"
+}
+
+do_alpine() {
+    alpine_install_shellcheck
+    alpine_clear_up
 }
 
 deb_install_shellcheck() {
-    if [ -x "$(which apt)" ]; then
-        apt update
-        apt install -y --no-install-recommends shellcheck
-        exit 0
+    apt update
+    apt install -y --no-install-recommends shellcheck
+}
+
+do_debian() {
+    deb_install_shellcheck
+}
+
+choose_and_execute_installation() {
+    if [ -x "$(which apk)" ]; then
+        do_alpine
+    elif [ -x "$(which apt)" ]; then
+        do_debian
+    else
+        w_echo 'This script only work on Debian, Alpine'
     fi
 }
 
-install_shellcheck() {
-    if [ ! -x "$(which shellcheck)" ]; then
-        alpine_install_shellcheck
-        deb_install_shellcheck
-        w_echo 'This script only work on Debian, Alpine'
+check_installation_result(){
+    if shellcheck_is_installed; then
+        i_echo 'ShellCheck installed successfully'
+        exit 0
     else
-        w_echo 'ShellCheck already installed'
+        e_echo 'ShellCheck installation failed. =/'
+        exit 1
     fi
 }
 
@@ -62,9 +92,12 @@ main() {
     exit_is_not_superuser
     i_echo "Install ShellCheck"
 
-    install_shellcheck
-
-    i_echo 'ShellCheck installed successfully'
+    if shellcheck_is_installed; then
+        choose_and_execute_installation
+        check_installation_result
+    else
+        w_echo 'ShellCheck already installed'
+    fi
 }
 
 main "$@"
